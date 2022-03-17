dbc3a5c2faee695a74def022e3b76cda220bc981
ggarber42 <ggarber@protonmail.com>
2022-03-17 18:07:28 -0300

    passing values through provider

--- lib/screens/product_overview.dart
+++ lib/screens/product_overview.dart
@@ -1,17 +1,14 @@
 import 'package:flutter/material.dart';
 
 import '../widgets/products_grid.dart';
-import '../models/product.dart';
-import '../utils/dummy_data.dart';
 
 class ProductOverview extends StatelessWidget {
-  final List<Product> loadedProducts = DUMMY_PRODUCTS;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('My Shop')),
-      body: ProductGrid(loadedProducts: loadedProducts),
+      body: ProductsGrid(),
     );
   }
 }
--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -1,30 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import '../models/product.dart';
 import '../widgets/product_item.dart';
+import '../providers/products.dart';
 
-class ProductGrid extends StatelessWidget {
-  const ProductGrid({
-    Key? key,
-    required this.loadedProducts,
-  }) : super(key: key);
-
-  final List<Product> loadedProducts;
+class ProductsGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.items;
     return GridView.builder(
         padding: const EdgeInsets.all(10),
-        itemCount: loadedProducts.length,
+        itemCount: products.length,
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
             childAspectRatio: 3 / 2,
             crossAxisSpacing: 10,
             mainAxisSpacing: 10),
         itemBuilder: (ctx, i) => ProductItem(
-              loadedProducts[i].id,
-              loadedProducts[i].title,
-              loadedProducts[i].imageUrl,
+              products[i].id,
+              products[i].title,
+              products[i].imageUrl,
             ));
   }
 }
