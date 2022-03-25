75b91e0298b21bb7118f74c5b5e34e41ee63776b
ggarber42 <ggarber@protonmail.com>
2022-03-25 18:23:56 -0300

    adicionando varios providers

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import './screens/product_detail.dart';
 import './screens/product_overview.dart';
 import './providers/products.dart';
+import '../providers/cart.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,19 +14,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     /* quando criamos um objeto, usar o create */
-    return ChangeNotifierProvider(
-      create: (ctx) => Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Cart(),
+        )
+      ],
       child: MaterialApp(
         title: 'MyShopp',
         theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.deepOrange,
-          fontFamily: 'Lato'
-        ),
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            fontFamily: 'Lato'),
         home: ProductOverview(),
-        routes: {
-          ProductDetail.routeName: (ctx) => ProductDetail()
-        },
+        routes: {ProductDetail.routeName: (ctx) => ProductDetail()},
       ),
     );
   }
