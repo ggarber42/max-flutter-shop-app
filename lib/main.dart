0c5107d25b251879e0665161ea9d78fa40da55e5
ggarber42 <ggarber@protonmail.com>
2022-03-29 20:31:01 -0300

    tirando banner debug

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
         )
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'MyShopp',
         theme: ThemeData(
             primarySwatch: Colors.purple,
