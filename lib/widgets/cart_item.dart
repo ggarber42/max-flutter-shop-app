a80084ba290347dad7c176671cc7fd8df923f5ce
ggarber42 <ggarber@protonmail.com>
2022-03-31 20:42:54 -0300

    adicionando widget do slide que joga fora

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,29 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/cart.dart';
 
 class CartItem extends StatelessWidget {
   final String id;
+  final String productId;
   final double price;
   final int quantity;
   final String title;
 
-  CartItem(this.id, this.price, this.quantity, this.title);
+  CartItem(this.id, this.productId, this.price, this.quantity, this.title);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-      child: Padding(
-          padding: EdgeInsets.all(10),
-          child: ListTile(
-            leading: CircleAvatar(
-              child: Padding(
-                  padding: EdgeInsets.all(5),
-                  child: FittedBox(child: Text('\$ $price'))),
-            ),
-            title: Text(title),
-            subtitle: Text('Total: \$${(price * quantity)}'),
-            trailing: Text('$quantity'),
-          )),
+    return Dismissible(
+      direction: DismissDirection.endToStart,
+      key: ValueKey(id),
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: Icon(Icons.delete, color: Colors.white, size: 40),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+      ),
+      child: Card(
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: Padding(
+            padding: EdgeInsets.all(10),
+            child: ListTile(
+              leading: CircleAvatar(
+                child: Padding(
+                    padding: EdgeInsets.all(5),
+                    child: FittedBox(child: Text('\$ $price'))),
+              ),
+              title: Text(title),
+              subtitle: Text('Total: \$${(price * quantity)}'),
+              trailing: Text('$quantity'),
+            )),
+      ),
+      onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
+      },
     );
   }
 }
